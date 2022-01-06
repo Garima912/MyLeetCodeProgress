676b1a972ee188ff9fcc0091dc06dc7b4611d242
Garima Chaudhary <60899474+Garima912@users.noreply.github.com>
2022-01-06 01:36:33 -0600

    Time: 180 ms (10.35%), Space: 42.4 MB (97.06%) - LeetHub

--- 648-replace-words/648-replace-words.java
+++ 648-replace-words/648-replace-words.java
@@ -10,7 +10,7 @@ class Solution {
         List<String> sentenceList = new ArrayList<String>(Arrays.asList(sentence.split(" ")));
         
         for(int i =0; i< sentenceList.size();i++){
-            for(int j =1; j < sentenceList.get(i).length();j++){
+            for(int j =1; j <= sentenceList.get(i).length();j++){
                 String prefix = sentenceList.get(i).substring(0,j);
                 if(rootMap.containsKey(prefix)){
                     sentenceList.set(i,prefix);
