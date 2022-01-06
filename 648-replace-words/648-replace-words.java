a7f368d61fc8bba54ca560c279571d5c42f0402b
Garima Chaudhary <60899474+Garima912@users.noreply.github.com>
2022-01-06 01:33:57 -0600

    Time: 458 ms (5.03%), Space: 118.9 MB (5.03%) - LeetHub

---
+++ 648-replace-words/648-replace-words.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String replaceWords(List<String> dictionary, String sentence) {
+        
+        HashMap<String, Integer> rootMap = new HashMap <> ();
+        
+        for(String root: dictionary){
+            rootMap.put(root, root.length());
+        }
+        
+        List<String> sentenceList = new ArrayList<String>(Arrays.asList(sentence.split(" ")));
+        
+        for(int i =0; i< sentenceList.size();i++){
+            for(int j =0; j < sentenceList.get(i).length();j++){
+                String prefix = sentenceList.get(i).substring(0,j);
+                if(rootMap.containsKey(prefix)){
+                    sentenceList.set(i,prefix);
+                    break;
+                }
+            }
+            
+        }
+        
+        return String.join(" ", sentenceList);
+        
+    }
+}
