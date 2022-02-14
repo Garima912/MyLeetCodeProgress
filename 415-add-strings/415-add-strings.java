2a973a08d8edb160876395287ee4a93ac7029976
Garima Chaudhary <60899474+Garima912@users.noreply.github.com>
2022-02-14 02:23:52 -0600

    Time: 92 ms (5.17%), Space: 73.1 MB (5.14%) - LeetHub

---
+++ 415-add-strings/415-add-strings.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String addStrings(String num1, String num2) {
+        String result = "";
+        int p =num1.length()-1, q = num2.length()-1;
+        int carry =0;
+        int digit =0;
+        
+        
+        while(p >=0 || q >=0){
+            int a = p>=0 ?Character.getNumericValue(num1.charAt(p)):0;
+            int b = q>=0? Character.getNumericValue(num2.charAt(q)):0;
+            int sum = carry + a +b;
+            System.out.println(a + "+" +b);
+            digit = sum%10;
+            carry = sum/10;
+            result = digit + result;
+            p--;
+            q--;
+        }
+        if(carry > 0){
+            result = "1" + result;
+        }
+        
+        return result;
+        
+        
+    }
+}
